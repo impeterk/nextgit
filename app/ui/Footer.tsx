ec780bfb7454a162dcbd199d4af5dabe4aa1dcba
Peter <storm.sk123@gmail.com>
2023-12-30 22:34:57 +0100

    updated loading page

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -2,8 +2,8 @@ import About from "./footer/About";
 
 export default function Footer() {
     return (
-        <footer className="container text-center bg-transparent py-6 ">
-        <small className="text-muted-foreground">Peter Kudeláš {new Date().getFullYear()} | </small>
+        <footer className="container text-center bg-transparent py-6 flex items-center justify-center gap-2">
+        <p className="text-muted-foreground">Peter Kudeláš {new Date().getFullYear()} |</p>
         <About />
         </footer>
     )
