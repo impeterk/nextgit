d5d5a077b232df4ab00debec5ef98cedcf836ffa
peter <peterkudelas@gmail.com>
2023-12-31 15:19:47 +0100

    autorotate button

--- app/[user]/[year]/Scene.tsx
+++ app/[user]/[year]/Scene.tsx
@@ -11,6 +11,7 @@ import {
 import { Canvas, Vector3 } from "@react-three/fiber";
 import { ReactNode, useState, useEffect } from "react";
 import { useSpring, animated, easings } from "@react-spring/three";
+import { Button } from "@/components/ui/button";
 
 function Box({
   position,
@@ -63,6 +64,7 @@ export default function Scene({
   totalContributions: number;
 }) {
   const [scale, setScale] = useState(0);
+  const [autoRotate, setAutoRotate] = useState(true);
 
   function normalize(count: number, base = 4, offset = 2) {
     switch (true) {
@@ -79,92 +81,117 @@ export default function Scene({
 
   const { scaleY } = useSpring({
     scaleY: scale,
+
     config: {
       tension: 300,
       friction: 150,
       easing: easings.easeInOutQuad,
+      duration: 1500,
     },
   });
+
   useEffect(() => {
     setScale(1);
   }, []);
 
   return (
-    <Canvas className="h-full w-full">
-      <Grid
-        infiniteGrid
-        sectionColor="#1e40af"
-        sectionSize={20}
-        cellSize={20}
-        fadeDistance={800}
-      />
-      <PerspectiveCamera makeDefault position={[10, 150, 400]} fov={75}>
-        <OrbitControls enableDamping autoRotate autoRotateSpeed={0.65} />
-      </PerspectiveCamera>
-      <Stars
-        radius={600}
-        depth={50}
-        count={50000}
-        factor={4}
-        saturation={1}
-        fade
-        speed={1}
-      />
-      <ambientLight color="#fff" intensity={0.4} />
-      <directionalLight position={[0, 200, 200]} intensity={2} color="#fff" />
-      <directionalLight position={[0, 200, -200]} color="#fff" intensity={2} />
-      
-      <Center top>
-      <Box position={[0,0,0]} height={[53*12, 50, 7*14]} level={0}>
-      <Center position-z={4*14-1}>
-        <Text3D
-          curveSegments={32}
-          bevelEnabled
-          bevelSize={1}
-          bevelThickness={5}
-          size={30}
-          font="/Inter_Bold.json"
-        >
-          {totalContributions} Contributions
-          <meshStandardMaterial color="azure" />
-        </Text3D>
-      </Center>
-      </Box>
-      </Center>
-      <Center top position={[0, 50, 0]}>
-        {contributions.map((row: any, i: number) =>
-          row.map((day: any, j: number) => (
-            <animated.group
-              position={[0, 0, 12 * (i || 1)]}
-              key={`${day?.day} + ${day?.month}`}
-              scale-y={scaleY}
-            >
-              <Box
-                position={[12 * (j || 1), normalize(day?.count) / 2, 0]}
-                height={[10, normalize(day?.count), 10]}
-                level={day?.level}
-                key={j}
+    <div className="relative h-full w-full">
+      <Button
+        className="absolute z-50 bottom-20 left-1/2 -translate-x-1/2"
+        variant={"secondary"}
+        onClick={() => setAutoRotate(!autoRotate)}
+      >
+        Auto Rotate {autoRotate ? " ✔" : " ✖"}
+      </Button>
+
+      <Canvas className="h-full w-full">
+        <Grid
+          infiniteGrid
+          sectionColor="#1e40af"
+          sectionSize={20}
+          cellSize={20}
+          fadeDistance={800}
+        />
+        <PerspectiveCamera makeDefault position={[10, 150, 400]} fov={75}>
+          <OrbitControls
+            enableDamping
+            autoRotate={autoRotate}
+            autoRotateSpeed={0.65}
+          />
+        </PerspectiveCamera>
+        <Stars
+          radius={600}
+          depth={50}
+          count={50000}
+          factor={4}
+          saturation={1}
+          fade
+          speed={1}
+        />
+        <ambientLight color="#fff" intensity={0.4} />
+        <directionalLight position={[0, 200, 200]} intensity={2} color="#fff" />
+        <directionalLight
+          position={[0, 200, -200]}
+          color="#fff"
+          intensity={2}
+        />
+
+        <animated.group position={[0, 0, 5]}>
+          <Center top>
+            <mesh>
+              <boxGeometry args={[53 * 12, 50, 7 * 14]} />
+              <meshLambertMaterial attach="material" color="#1E293B" />
+              <Center position-z={4 * 14 - 3}>
+                <Text3D
+                  curveSegments={32}
+                  bevelEnabled
+                  bevelSize={1}
+                  bevelThickness={5}
+                  size={30}
+                  font="/Inter_Bold.json"
+                >
+                  {totalContributions} Contributions
+                  <meshStandardMaterial color="azure" />
+                </Text3D>
+              </Center>
+            </mesh>
+          </Center>
+        </animated.group>
+        <Center top position={[0, 50, 0]}>
+          {contributions.map((row: any, i: number) =>
+            row.map((day: any, j: number) => (
+              <animated.group
+                position={[0, 0, 12 * (i || 1)]}
+                key={`${day?.day} + ${day?.month}`}
+                scale-y={scaleY}
               >
-                {day?.count && (
-                  <Center position={[0, normalize(day?.count) / 2 + 5, 0]}>
-                    <Text3D
-                      curveSegments={32}
-                      bevelEnabled
-                      bevelSize={0.04}
-                      bevelThickness={1}
-                      size={5}
-                      font="/Inter_Bold.json"
-                    >
-                      {day?.count}
-                      <meshStandardMaterial color="azure" />
-                    </Text3D>
-                  </Center>
-                )}
-              </Box>
-            </animated.group>
-          ))
-        )}
-      </Center>
-    </Canvas>
+                <Box
+                  position={[12 * (j || 1), normalize(day?.count) / 2, 0]}
+                  height={[10, normalize(day?.count), 10]}
+                  level={day?.level}
+                  key={j}
+                >
+                  {day?.count && (
+                    <Center position={[0, normalize(day?.count) / 2 + 5, 0]}>
+                      <Text3D
+                        curveSegments={32}
+                        bevelEnabled
+                        bevelSize={0.04}
+                        bevelThickness={1}
+                        size={5}
+                        font="/Inter_Bold.json"
+                      >
+                        {day?.count}
+                        <meshStandardMaterial color="azure" />
+                      </Text3D>
+                    </Center>
+                  )}
+                </Box>
+              </animated.group>
+            ))
+          )}
+        </Center>
+      </Canvas>
+    </div>
   );
 }
--- app/ui/footer/About.tsx
+++ app/ui/footer/About.tsx
@@ -1,61 +1,176 @@
 import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-  } from "@/components/ui/drawer"
-  import { Button } from "@/components/ui/button"  
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
 
 export default function About() {
-    return (
-        <Drawer>
-  <DrawerTrigger>About</DrawerTrigger>
-  <DrawerContent>
-    <DrawerHeader className="w-full max-w-md mx-auto space-y-12">
-        <div className="space-y-2">
-      <DrawerTitle>About </DrawerTitle>
-      <DrawerDescription>You can see your your contributions to GitHub in 3D. This was heavily influenced and inspired by <a href="http://joyofcode.xyz" target="_blank" rel="noopener noreferrer" className="underline underline-offset-4 text-accent-foreground">Joy of Code</a> videos about theirs implementation of Github Skyline. However I have decided to create this in <a href="http://nextjs.org" className="underline text-accent-foreground underline-offset-4 " target="_blank" rel="noopener noreferrer">Next.js</a></DrawerDescription>
-      </div>
-      <div className="space-y-2">
-      <DrawerTitle>Created with</DrawerTitle>
-      <DrawerDescription><div className="flex text-lg gap-4">
-        <a className="underline text-accent-foreground underline-offset-4 "  href="http://nextjs.org" target="_blank" rel="noopener noreferrer">Next js</a>
-        <a className="underline text-accent-foreground underline-offset-4 "  href="http://threejs.org" target="_blank" rel="noopener noreferrer">Three Js</a>
-        <a className="underline text-accent-foreground underline-offset-4 "  href="http://https://docs.pmnd.rs/react-three-fiber/getting-started/introduction" target="_blank" rel="noopener noreferrer">React Three Fiber</a>
-        <a className="underline text-accent-foreground underline-offset-4 "  href="https://ui.shadcn.com/" target="_blank" rel="noopener noreferrer">Shad CN</a>
-        </div>
-        </DrawerDescription>
-        </div>
-        <div className="space-y-2">
-      <DrawerTitle>Api</DrawerTitle>
-      <DrawerDescription className="space-y-2" >
-        <p>You can also use API endpoint to fetch json data.</p>
-        <p><code className="bg-muted-foreground font-mono px-2 py-1 text-muted rounded">/api/gh?user=[user]&year=[year]</code></p>
-        <p>Response</p>
-        <div className="w-full bg-muted-foreground font-mono px-2 py-1 text-muted rounded my-1">
-        <p><code>{`{totalContributions: number,`}<br/>
-        {`
+  return (
+    <Drawer>
+      <DrawerTrigger>About</DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="w-full max-w-md mx-auto space-y-12">
+          <div className="space-y-2">
+            <DrawerTitle>About </DrawerTitle>
+            <DrawerDescription>
+              You can see your your contributions to GitHub in 3D. This was
+              heavily influenced and inspired by{" "}
+              <a
+                href="https://joyofcode.xyz"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline underline-offset-4 text-accent-foreground"
+              >
+                Joy of Code
+              </a>{" "}
+              videos about theirs implementation of Github Skyline. However I
+              have decided to create this in{" "}
+              <a
+                href="https://nextjs.org"
+                className="underline text-accent-foreground underline-offset-4 "
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Next.js
+              </a>
+            </DrawerDescription>
+          </div>
+          <div className="space-y-2">
+            <DrawerTitle>Created with</DrawerTitle>
+            <DrawerDescription>
+              <div className="flex text-lg gap-4">
+                <a
+                  className="underline text-accent-foreground underline-offset-4 "
+                  href="https://nextjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Next js
+                </a>
+                <a
+                  className="underline text-accent-foreground underline-offset-4 "
+                  href="https://threejs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Three js
+                </a>
+                <a
+                  className="underline text-accent-foreground underline-offset-4 "
+                  href="https://docs.pmnd.rs/react-three-fiber/getting-started/introduction"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  React Three Fiber
+                </a>
+                <a
+                  className="underline text-accent-foreground underline-offset-4 "
+                  href="https://ui.shadcn.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  shadcn/ui
+                </a>
+              </div>
+            </DrawerDescription>
+          </div>
+          <div className="space-y-2">
+            <DrawerTitle>Api</DrawerTitle>
+            <DrawerDescription className="space-y-2">
+              <p>You can also use API endpoint to fetch json data.</p>
+              <p>
+                <code className="bg-muted-foreground font-mono px-2 py-1 text-muted rounded">
+                  /api/gh?user=[user]&year=[year]
+                </code>
+              </p>
+              <p>Response</p>
+              <div className="w-full bg-muted-foreground font-mono px-2 py-1 text-muted rounded my-1">
+                <p>
+                  <code>
+                    {`{totalContributions: number,`}
+                    <br />
+                    {`
         contributions: ({
             count: number;
             day: string;
             month: string;
             year: string;
             level: number;
-        } | null)[][]})}`}</code></p>
-</div>
-        </DrawerDescription>
-        </div>
-    </DrawerHeader>
-    <DrawerFooter>
-      <DrawerClose>
-        <Button variant="outline">Close</Button>
-      </DrawerClose>
-    </DrawerFooter>
-  </DrawerContent>
-</Drawer>
-    )
+        } | null)[][]})}`}
+                  </code>
+                </p>
+              </div>
+            </DrawerDescription>
+          </div>
+          <div>
+            <hr />
+            <DrawerTitle className="mt-2">Contact</DrawerTitle>
+            <DrawerDescription className="text-accent-foreground">
+              <div className="flex justify-center gap-8 items-center">
+                <p>
+                  <a
+                    href="https://www.peterkudelas.eu"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-lg underline underline-offset-2"
+                  >
+                    peterkudelas.eu
+                  </a>
+                </p>
+                <p>
+                  <a
+                    href="https://github.com/impeterk/nextgit"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"
+                      />
+                    </svg>
+                  </a>
+                </p>
+                <p>
+                  <a
+                    href="https://www.linkedin.com/in/impeterk/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"
+                      />
+                    </svg>
+                  </a>
+                </p>
+              </div>
+            </DrawerDescription>
+          </div>
+        </DrawerHeader>
+        <DrawerFooter>
+          <DrawerClose>
+            <Button variant="outline">Close</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
 }
