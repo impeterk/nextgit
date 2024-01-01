798925abc52d2569aa209396ebfebd4e18e154fd
Peter Kudelas <peterkudelas@gmail.com>
2024-01-01 14:55:56 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import {redirect} from 'next/navigation'
 
 export default async function Home() {
 
-    let year = new Date().getFullYear() - 1 
-    year = year >= 2023 ? year : 2023
+const year = new Date().getFullYear() - 1
 return  redirect(`/impeterk/${year}`)
 }
  
